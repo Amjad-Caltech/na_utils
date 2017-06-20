2f0f7ca3689e879ec13b778f04cb1ebb16f7dd71
Amjad <amjad@caltech.edu>
2017-06-20 11:28:34 -0700

    initial commit of RevCom.py

---
+++ RevCom.py
@@ -0,0 +1,21 @@
+def RevCom(seq):
+    """This functions returns the reverse complement of the 'seq'"""
+    # initialize
+    RC = ''
+
+    for base in seq[::-1]:
+        RC += complment(base)
+
+    return RC
+
+def complment(base):
+    if base in 'Aa':
+        return 'T'
+    elif base in 'Tt':
+        return 'A'
+    elif base in 'Cc':
+        return 'G'
+    elif base in 'Gg':
+        return 'C'
+    else:
+        return '((Error! Wrong base.))'
